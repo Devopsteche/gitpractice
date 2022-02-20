72998446faa0edb96f3ad1e4bb667e95e5c33260
sajin appukuttan <sajin.net@gmail.com>
2022-02-20 11:38:18 +0100

    rebase test

--- main.tf
+++ main.tf
@@ -4,3 +4,11 @@ resource "aws_s3_bucket" "my_s3" {
 resource "aci_cloud_domain_profile" "" {
 
 }
+
+resource "aws_iam_role" "" {
+  assume_role_policy = ""
+}
+
+resource "aws_vpc" "" {
+  cidr_block = ""
+}
